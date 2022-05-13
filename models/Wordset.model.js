b4d00627923d7e0e1446785d8fb16568cdef8aff
unknown <cesyrub@gmail.com>
2022-05-13 20:35:59 +0200

    add private property and btn on create

--- models/Wordset.model.js
+++ models/Wordset.model.js
@@ -13,6 +13,10 @@ const wordSetSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    private: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
