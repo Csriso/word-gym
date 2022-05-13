d11198b76fabf314b06e9cde2684c40ef5c97ed0
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-13 19:06:06 +0200

    populate methods. Adding user to created collection

--- routes/collection.routes.js
+++ routes/collection.routes.js
@@ -5,7 +5,8 @@ const WordSetModel = require("../models/Wordset.model");
 
 router.get("/", async (req, res, next) => {
   try {
-    const wordSets = await WordSetModel.find();
+    const wordSets = await WordSetModel.find().populate('user');
+    console.log(wordSets)
     res.render("wordset/allsets.hbs", { wordSets });
   } catch (err) {
     console.log(err);
@@ -14,7 +15,7 @@ router.get("/", async (req, res, next) => {
 
 router.get("/my", isLoggedIn, async (req, res, next) => {
   try {
-    const wordSets = await WordSetModel.find();
+    const wordSets = await WordSetModel.find().populate('user');;
     res.render("wordset/allsets.hbs", { wordSets, myCollections: true });
   } catch (err) {
     console.log(err);
@@ -27,7 +28,8 @@ router.get("/create", isLoggedIn, (req, res, next) => {
 
 router.post("/create", isLoggedIn, (req, res, next) => {
   console.log(req.body);
-  WordSetModel.create(req.body)
+  const { name } = req.body
+  WordSetModel.create( {"name": name, "user": req.session.user})
     .then((response) => {
       res.redirect("/collection/my");
     })
