67ba4e6c522def1574e63d7d76883614b4da7b75
unknown <cesyrub@gmail.com>
2022-05-16 15:14:03 +0200

    some styles on train and audio recorder

--- public/js/audioRecorder.js
+++ public/js/audioRecorder.js
@@ -1,10 +1,10 @@
 // set up basic variables for app
 
-const record = document.querySelector('.record');
-const stop = document.querySelector('.stop');
-const soundClips = document.querySelector('.sound-clips');
-const canvas = document.querySelector('.visualizer');
-const mainSection = document.querySelector('.main-controls');
+const record = document.querySelector(".record");
+const stop = document.querySelector(".stop");
+const soundClips = document.querySelector(".sound-clips");
+const canvas = document.querySelector(".visualizer");
+const mainSection = document.querySelector(".main-controls");
 
 // disable stop button while not recording
 
@@ -18,17 +18,17 @@ const canvasCtx = canvas.getContext("2d");
 //main block for doing the audio recording
 
 if (navigator.mediaDevices.getUserMedia) {
-  console.log('getUserMedia supported.');
+  console.log("getUserMedia supported.");
 
   const constraints = { audio: true };
   let chunks = [];
 
-  let onSuccess = function(stream) {
+  let onSuccess = function (stream) {
     const mediaRecorder = new MediaRecorder(stream);
 
     visualize(stream);
 
-    record.onclick = function() {
+    record.onclick = function () {
       mediaRecorder.start();
       console.log(mediaRecorder.state);
       console.log("recorder started");
@@ -36,9 +36,9 @@ if (navigator.mediaDevices.getUserMedia) {
 
       stop.disabled = false;
       record.disabled = true;
-    }
+    };
 
-    stop.onclick = function() {
+    stop.onclick = function () {
       mediaRecorder.stop();
       console.log(mediaRecorder.state);
       console.log("recorder stopped");
@@ -48,28 +48,30 @@ if (navigator.mediaDevices.getUserMedia) {
 
       stop.disabled = true;
       record.disabled = false;
-    }
+    };
 
-    mediaRecorder.onstop = function(e) {
+    mediaRecorder.onstop = function (e) {
       console.log("data available after MediaRecorder.stop() called.");
 
       //const clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
+      document.querySelector(".sound-clips").innerHTML = "";
+      const clipContainer = document.createElement("article");
+      const clipLabel = document.createElement("p");
+      const audio = document.createElement("audio");
+      const deleteButton = document.createElement("button");
 
-      const clipContainer = document.createElement('article');
-      const clipLabel = document.createElement('p');
-      const audio = document.createElement('audio');
-      const deleteButton = document.createElement('button');
-
-      clipContainer.classList.add('clip');
-      audio.setAttribute('controls', '');
-      deleteButton.textContent = 'Delete';
-      deleteButton.className = 'delete';
-
-    //   if(clipName === null) {
-    //     clipLabel.textContent = 'My unnamed clip';
-    //   } else {
-    //     clipLabel.textContent = clipName;
-    //   }
+      clipContainer.classList.add("clip");
+      audio.setAttribute("controls", "");
+      deleteButton.textContent = "Delete";
+      deleteButton.className =
+        "delete focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-5";
+      clipContainer.className =
+        "flex flex-col justify-center items-center mt-5";
+      //   if(clipName === null) {
+      //     clipLabel.textContent = 'My unnamed clip';
+      //   } else {
+      //     clipLabel.textContent = clipName;
+      //   }
 
       clipContainer.appendChild(audio);
       clipContainer.appendChild(clipLabel);
@@ -77,46 +79,44 @@ if (navigator.mediaDevices.getUserMedia) {
       soundClips.appendChild(clipContainer);
 
       audio.controls = true;
-      const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
       chunks = [];
       const audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
       console.log("recorder stopped");
 
-      deleteButton.onclick = function(e) {
+      deleteButton.onclick = function (e) {
         let evtTgt = e.target;
         evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-      }
+      };
 
-    //   clipLabel.onclick = function() {
-    //     const existingName = clipLabel.textContent;
-    //     const newClipName = prompt('Enter a new name for your sound clip?');
-    //     if(newClipName === null) {
-    //       clipLabel.textContent = existingName;
-    //     } else {
-    //       clipLabel.textContent = newClipName;
-    //     }
-    //   }
+      //   clipLabel.onclick = function() {
+      //     const existingName = clipLabel.textContent;
+      //     const newClipName = prompt('Enter a new name for your sound clip?');
+      //     if(newClipName === null) {
+      //       clipLabel.textContent = existingName;
+      //     } else {
+      //       clipLabel.textContent = newClipName;
+      //     }
+      //   }
+    };
 
-    }
-
-    mediaRecorder.ondataavailable = function(e) {
+    mediaRecorder.ondataavailable = function (e) {
       chunks.push(e.data);
-    }
-  }
+    };
+  };
 
-  let onError = function(err) {
-    console.log('The following error occured: ' + err);
-  }
+  let onError = function (err) {
+    console.log("The following error occured: " + err);
+  };
 
   navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
-
 } else {
-   console.log('getUserMedia not supported on your browser!');
+  console.log("getUserMedia not supported on your browser!");
 }
 
 function visualize(stream) {
-  if(!audioCtx) {
+  if (!audioCtx) {
     audioCtx = new AudioContext();
   }
 
@@ -130,34 +130,32 @@ function visualize(stream) {
   source.connect(analyser);
   //analyser.connect(audioCtx.destination);
 
-  draw()
+  draw();
 
   function draw() {
-    const WIDTH = canvas.width
+    const WIDTH = canvas.width;
     const HEIGHT = canvas.height;
 
     requestAnimationFrame(draw);
 
     analyser.getByteTimeDomainData(dataArray);
 
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillStyle = "rgb(200, 200, 200)";
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
     canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.strokeStyle = "rgb(0, 0, 0)";
 
     canvasCtx.beginPath();
 
-    let sliceWidth = WIDTH * 1.0 / bufferLength;
+    let sliceWidth = (WIDTH * 1.0) / bufferLength;
     let x = 0;
 
-
-    for(let i = 0; i < bufferLength; i++) {
-
+    for (let i = 0; i < bufferLength; i++) {
       let v = dataArray[i] / 128.0;
-      let y = v * HEIGHT/2;
+      let y = (v * HEIGHT) / 2;
 
-      if(i === 0) {
+      if (i === 0) {
         canvasCtx.moveTo(x, y);
       } else {
         canvasCtx.lineTo(x, y);
@@ -166,14 +164,13 @@ function visualize(stream) {
       x += sliceWidth;
     }
 
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
     canvasCtx.stroke();
-
   }
 }
 
-window.onresize = function() {
+window.onresize = function () {
   canvas.width = mainSection.offsetWidth;
-}
+};
 
 window.onresize();
