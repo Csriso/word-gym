4548e1181ee923a8a97822fb92a0d3ca7fc4f958
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-18 11:14:23 +0200

    edit button my collection

--- app.js
+++ app.js
@@ -25,21 +25,19 @@ hbs.registerHelper("capitalize", function (str) {
   return str[0].toUpperCase() + str.slice(1);
 });
 
-hbs.registerHelper("when", (operand_1, operator, operand_2, options) => {
-  let operators = {                     //  {{#when <operand1> 'eq' <operand2>}}
-    'eq': (l,r) => l == r,              //  {{/when}}
-    'noteq': (l,r) => l != r,
-    'gt': (l,r) => (+l) > (+r),                        // {{#when var1 'eq' var2}}
-    'gteq': (l,r) => ((+l) > (+r)) || (l == r),        //               eq
-    'lt': (l,r) => (+l) < (+r),                        // {{else when var1 'gt' var2}}
-    'lteq': (l,r) => ((+l) < (+r)) || (l == r),        //               gt
-    'or': (l,r) => l || r,                             // {{else}}
-    'and': (l,r) => l && r,                            //               lt
-    '%': (l,r) => (l % r) === 0                        // {{/when}}
+hbs.registerHelper( "when",function(operand_1, operator, operand_2, options) {
+  var operators = {
+   'eq': function(l,r) { return l == r; },
+   'noteq': function(l,r) { return l != r; },
+   'gt': function(l,r) { return Number(l) > Number(r); },
+   'or': function(l,r) { return l || r; },
+   'and': function(l,r) { return l && r; },
+   '%': function(l,r) { return (l % r) === 0; }
   }
-  let result = operators[operator](operand_1,operand_2);
-  if(result) return options.fn(this); 
-  return options.inverse(this);       
+  , result = operators[operator](operand_1,operand_2);
+
+  if (result) return options.fn(this);
+  else  return options.inverse(this);
 });
 
 const app = express();
