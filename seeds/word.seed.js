16e468c8961bcae338909a7d62b0ec2ec5953c8c
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-19 15:49:57 +0200

    word.seed working

--- seeds/word.seed.js
+++ seeds/word.seed.js
@@ -5,9 +5,7 @@ const mongoose = require("mongoose");
 const WordModel = require("../models/Word.model")
 const WordSetModel = require("../models/WordSet.model")
 const wordArray = require("./words.json")
-
-const axios = require('axios').default;
-const APIurl="https://api.dictionaryapi.dev/api/v2/entries/en/"
+const { getWordFromApi } = require("../utils/getWord");
 
 const deleteWords = async () => {
     try{
@@ -18,70 +16,27 @@ const deleteWords = async () => {
         console.log(err)
     }
 }
-const addWordsFromSeed = () => {
-        WordSetModel.deleteMany()
-        .then((response)=>{
-            return WordModel.deleteMany();
-        })
-        .then((response)=>{
-            return WordModel.insertMany(wordArray)
-        })
-        .then((response)=>{
-            console.log("response al 3r then: ", response)
-            return WordSetModel.create({name: "testing", words: response})
-        })
-        .then((response)=>{
-            console.log(response)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-}
-
-const addWordFromArray = (wordArray) => {
-    wordArray.forEach(word => {
-        addWord(word)
-    })
-}
-
-
-const addWord = (word) => {
-    getWordFromApi(word)
-    .then((response)=>{
-        return WordModel.create(response)
-    })
-    .then((response)=>{
-        console.log(response)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}
-
-const getWordFromApi = async (word) =>{
-    try{
-        const response = await axios.get(APIurl+word)
-        let phonetic=response.data[0].phonetics.find((elem)=>{
-            return elem.audio
-        })
-        const content = {
-            word: response.data[0].word,
-            audio: phonetic.audio,
-            //meanings: response.data[0].meanings
+const addWordsFromSeed = async (wordsArr, wordSetName) =>{
+    let actions = wordsArr.map((word)=>{
+        return getWordFromApi(word)
+      })
+      let results = await Promise.all(actions);
+      let foundWordsArr=[]
+      let notFoundWords=[]
+      results.forEach((elem, index) =>{
+        if (elem.audio !== undefined) {
+          foundWordsArr.push(elem.word);
+        } else {
+          notFoundWords.push(wordsArr[index]);
         }
-        return content
-    }catch(err){
-        console.log(err)
-    }
+      })
+  
+      wordSet = await WordSetModel.create(
+        {
+            name: wordSetName,
+            words: foundWordsArr, //wordsArr,
+            private: false,
+        }
+      );
 }
-/*
-Promise.allSettled([
-    addWords()
-]).then(response=>{})
-.catch((err)=>{
-    console.log(err)
-})
-*/
-addWordFromArray([
-    "Dog", "Cat", "House"
-])
+addWordsFromSeed(wordArray, "test4")
