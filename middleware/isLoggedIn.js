43b2b22330ec176b7f72492c98ab056dbcbcf23f
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-19 18:38:38 +0200

    double security for the presentation, mycollection

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -2,9 +2,8 @@ module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.user) {
     return res.redirect("/auth/login");
-    res.locals.user = undefined
   }
   req.user = req.session.user;
-  res.locals.user = req.session.user
+  //res.locals.user = req.session.user
   next();
 };
